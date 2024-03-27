da060fc7500c93bac5657ffb221f75bf62d7bcf0
andrew23 <cosmopablocarvajal@gmail.com>
2024-03-26 19:17:44 -0500

    se actualiza y mejoran las tarjetas de usuarios
    se actualiza y mejoran las tarjetas de usuarios para mostrar los datos de los usuarios , falta aun añadir la foto de perfil y acciones adicionales a las tarjetas

--- frontend/js/ListarUsuariosTarjetas.js
+++ frontend/js/ListarUsuariosTarjetas.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     obtenerDatosUsuarios();
   });
-  
+
+  function calcularRol(id_rol) {
+    return id_rol === 1 ? "ADMIN" : "ASOCIADO";
+  }
+
   async function obtenerDatosUsuarios() {
     try{
       const response = await fetch('/listarUsuarioSistema');
@@ -14,8 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
   
   function agregarDatosATabla(usuarios) {
     const tarjetasSection = document.getElementById('Tarjetas');
+    tarjetasSection.innerHTML = ''; // Limpiar el contenedor antes de agregar las tarjetas
 
     usuarios.forEach(usuario => {
+        const rol = calcularRol(usuario.rol_ingreso);
+
         const tarjetaDiv = document.createElement('div'); // Crear un div para cada tarjeta
         tarjetaDiv.classList.add('tarjeta'); // Agregar una clase para estilizar la tarjeta
 
@@ -27,13 +34,34 @@ document.addEventListener('DOMContentLoaded', () => {
             <p>Teléfono: ${usuario.telefono_persona}</p>
             <p>Correo: ${usuario.correo_persona}</p>
             <p>Usuario: ${usuario.usuario_ingreso}</p>
+            <p>Rol: ${rol}</p>
+        `;
+
+        const buttonsDiv = document.createElement('div'); // Crear un div para los botones
+        buttonsDiv.classList.add('buttons-container'); // Agregar una clase para estilizar el contenedor de botones
+
+        // Crear los botones y agregarlos al contenedor
+        const buttonsHTML = 
+        `
             <button class="btn btn-sm btn-primary" onclick="editarUsuario(${usuario.id})">
-                <i class="bi bi-pencil"></i> Editar
+                <i class="bi bi-pencil"></i> Crear Admin
+            </button> 
+            <button class="btn btn-sm btn-primary" onclick="editarUsuario(${usuario.id})">
+                <i class="bi bi-pencil"></i> Cambiar contraseña
+            </button> 
+
+            <button class="btn btn-sm btn-primary" onclick="editarUsuario(${usuario.id})">
+                <i class="bi bi-pencil"></i> Habilitar
             </button>
+
             <button class="btn btn-sm btn-danger" onclick="suspenderUsuario(${usuario.id})">
                 <i class="bi bi-x"></i> Suspender
             </button>
         `;
+        buttonsDiv.innerHTML = buttonsHTML;
+
+        // Agregar el contenedor de botones a la tarjeta
+        tarjetaDiv.appendChild(buttonsDiv);
 
         tarjetasSection.appendChild(tarjetaDiv); // Agregar la tarjeta al contenedor
     });
